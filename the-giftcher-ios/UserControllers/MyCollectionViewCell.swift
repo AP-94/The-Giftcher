5092373c46a463cacd69136e7a54bd834f1ba0e2
Alessandro Pace <alessandro_pace_daoa1819@cev.com>
2020-04-23 19:47:01 +0200

    Trabajando en los collectionView de HomeVC

---
+++ the-giftcher-ios/UserControllers/MyCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MyCollectionViewCell.swift
+//  the-giftcher-ios
+//
+//  Created by Alessandro Pace on 23/04/2020.
+//  Copyright © 2020 Rafael Juan Llabrés Socías. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var myLabel: UILabel!
+    
+}
